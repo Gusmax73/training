4b3c0201e6576ddaf827194a32a0baedd92bf1ea
Gusmax73 <gusmax73@yandex.ru>
2020-04-14 18:49:20 +0400

    Второй пункт задания. StringsTraining

--- app/src/main/java/com/example/user/javacoretraining/training/StringsTraining.java
+++ app/src/main/java/com/example/user/javacoretraining/training/StringsTraining.java
@@ -1,5 +1,7 @@
 package com.example.user.javacoretraining.training;
 
+import java.util.ArrayList;
+
 /**
  * Набор тренингов по работе со строками в java.
  * <p>
@@ -21,8 +23,11 @@ public class StringsTraining {
      * элементов строки text
      */
     public String getOddCharacterString(String text) {
-        //TODO: implement it
-        return "";
+        StringBuilder res = new StringBuilder();
+        for(int i=1; i < text.length(); i+=2) {
+            res.append(text.charAt(i));
+        }
+        return res.toString();
     }
 
     /**
@@ -36,8 +41,23 @@ public class StringsTraining {
      * вернуть пустой массив
      */
     public int[] getArrayLastSymbol(String text) {
-        //TODO: implement it
-        return new int[]{};
+        int j = 0;
+        for(int i=0; i < text.length()-1; i++) {
+            if(text.charAt(i) == text.charAt(text.length() - 1)) {
+                j++;
+            }
+        }
+
+        int[] res = new int[j];
+        j = 0;
+        for(int i=0; i < text.length()-1; i++) {
+            if(text.charAt(i) == text.charAt(text.length() - 1)) {
+                res[j] = i;
+                j++;
+            }
+        }
+
+        return res;
     }
 
     /**
@@ -48,8 +68,13 @@ public class StringsTraining {
      * @return количество цифр в строке
      */
     public int getNumbersCount(String text) {
-        //TODO: implement it
-        return 0;
+        int count = 0;
+        for(int i=0; i < text.length(); i++) {
+            if(Character.isDigit(text.charAt(i))) {
+                count ++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -60,8 +85,59 @@ public class StringsTraining {
      * @return текст, где цыфры заменены словами
      */
     public String replaceAllNumbers(String text) {
-        //TODO: implement it
-        return text;
+        String zero = "zero";
+        String one = "one";
+        String two = "two";
+        String three = "three";
+        String four = "four";
+        String five = "five";
+        String six = "six";
+        String seven = "seven";
+        String eight = "eight";
+        String nine = "nine";
+
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i < text.length(); i++) {
+            if(Character.isDigit(text.charAt(i))) {
+                switch (text.charAt(i)){
+                    case '0':
+                        res.append(zero);
+                        break;
+                    case '1':
+                        res.append(one);
+                        break;
+                    case '2':
+                        res.append(two);
+                        break;
+                    case '3':
+                        res.append(three);
+                        break;
+                    case '4':
+                        res.append(four);
+                        break;
+                    case '5':
+                        res.append(five);
+                        break;
+                    case '6':
+                        res.append(six);
+                        break;
+                    case '7':
+                        res.append(seven);
+                        break;
+                    case '8':
+                        res.append(eight);
+                        break;
+                    case '9':
+                        res.append(nine);
+                        break;
+                }
+            }
+            else {
+                res.append(text.charAt(i));
+            }
+        }
+
+        return res.toString();
     }
 
     /**
@@ -72,8 +148,16 @@ public class StringsTraining {
      * @return измененная строка
      */
     public String capitalReverse(String text) {
-        //TODO: implement it
-        return text;
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i < text.length(); i++) {
+            if(Character.isLowerCase(text.charAt(i))) {
+                res.append(Character.toUpperCase(text.charAt(i)));
+            }
+            else {
+                res.append(Character.toLowerCase(text.charAt(i)));
+            }
+        }
+        return res.toString();
     }
 
 }
